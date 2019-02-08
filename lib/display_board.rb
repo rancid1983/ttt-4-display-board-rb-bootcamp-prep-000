d27249c208d564f443bfc7f0a2335b7885e68dbc
andreas fürst <andreas_fuerst@bluewin.ch>
2019-02-08 10:29:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def
+display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+
+puts display_board
+
+board = [" "," "," "," "," "," "," "," "," "]
